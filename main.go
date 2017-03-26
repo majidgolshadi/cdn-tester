f851734f1719c89b3e8bc0ec2a8f46f5bbe7051b
Majid Golshadi <golshadi.majid@gmail.com>
2017-03-26 17:36:32 +0430

    CLI friendly

--- main.go
+++ main.go
@@ -7,57 +7,80 @@ import (
 	"net/http"
 	"io"
 	"time"
+	"flag"
 )
 
-func downloadFromUrl(url string) {
+func downloadFromUrl(url string) (int64, error) {
 	tokens := strings.Split(url, "/")
 	fileName := tokens[len(tokens)-1]
-	fmt.Println("Downloading", url, "to", fileName)
 
 	output, err := os.Create("download/" + fileName)
 	if err != nil {
-		fmt.Println("Error while creating", fileName, "-", err)
-		return
+		return 0, err
 	}
 	defer output.Close()
 
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error while downloading", url, "-", err)
-		return
+		return 0, err
 	}
 	defer response.Body.Close()
 
 	n, err := io.Copy(output, response.Body)
 	if err != nil {
-		fmt.Println("Error while downloading", url, "-", err)
-		return
+		return 0, err
 	}
 
-	fmt.Println(n, "bytes downloaded.")
+	return n, nil
 }
 
 func main() {
-	cdnUrl := "https://fs3.qmery.com/static_file/cdntest/test.jpg"
-	originUrl := "http://cdn3.fs.soroush-hamrah.ir/static_file/cdntest/test.jpg"
+	var cdnUrl = flag.String("cdn", "", "cdn url")
+	var try = flag.Int("t", 300, "download to cache in cdn edges")
+	var originUrl = flag.String("origin", "", "origin url")
+	var compareTime = flag.Int("c", 10, "compare time")
 
-	for i := 0; i < 300; i++ {
-		downloadFromUrl(cdnUrl)
+	flag.Parse()
+
+	if *cdnUrl == "" || *originUrl == "" {
+		println("Usage: -cdn <CDN_URL> -t <CDN_DOWNLOAD_TIME> -origin <ORIGIN_URL> -c <COMPARE_TIME>")
+		return
 	}
 
-	fmt.Printf("|%6s|%ds|", "cdn", "origin")
-	fmt.Printf("|%6s|%ds|", "------", "------")
-	for i := 0; i < 10; i++ {
+	fmt.Printf("\n origin url: %s\n cdn url: %s\n cdn download time: %d\n compare time: %d\n\n", *cdnUrl, *originUrl, *try, *compareTime)
+
+	for i := 0; i < *try; i++ {
+		if _, err := downloadFromUrl(*cdnUrl); err != nil {
+			fmt.Println("Error while downloading ", cdnUrl, " - ", err.Error())
+		} else {
+			fmt.Println(i+1, "downloaded")
+		}
+	}
+
+	fmt.Printf("\n|%12s|%12s|\n", "cdn", "origin")
+	fmt.Printf("|%12s|%12s|\n", "------------", "------------")
+	var err error
+	for i := 0; i < *compareTime; i++ {
 		downloadFromCdnStartedAt := time.Now().UnixNano()
-		downloadFromUrl(cdnUrl)
+		_, err = downloadFromUrl(*cdnUrl)
 		downloadFromCdnCompletedAt := time.Now().UnixNano()
 
+		if err != nil{
+			println(err.Error())
+			continue
+		}
+
 		downloadFromOriginStartedAt := time.Now().UnixNano()
-		downloadFromUrl(originUrl)
+		_, err = downloadFromUrl(*originUrl)
 		downloadFromOriginCompletedAt := time.Now().UnixNano()
+
+		if err != nil{
+			println(err.Error())
+			continue
+		}
 
 		cdnTime := downloadFromCdnCompletedAt - downloadFromCdnStartedAt
 		originTime := downloadFromOriginCompletedAt - downloadFromOriginStartedAt
-		fmt.Printf("|%6d|%6d|\n", cdnTime, originTime)
+		fmt.Printf("|%12d|%12d|\n", cdnTime, originTime)
 	}
 }
